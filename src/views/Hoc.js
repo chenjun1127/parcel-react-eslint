b15474ab1a5c972fe0f57974fff25970f84b5431
chenjun <402074940@qq.com>
2019-05-22 18:51:22 +0800

    :smile:

--- src/views/Hoc.js
+++ src/views/Hoc.js
@@ -6,9 +6,11 @@ class HocComponent extends React.Component {
       <div>
         <h1>
           高阶组件（HOC）是 React 中用于复用组件逻辑的一种高级技巧。HOC 自身不是 React API 的一部分，它是一种基于 React 的组合特性而形成的设计模式。 具体而言，<span>高阶组件是参数为组件，返回值为新组件的函数。</span>最大的特点就是重用组件逻辑。
-          <a href="javascript:;" onClick={this.props.handleClick} className={this.props.visible ? Styles.red : Styles.blue}>
-            click me
-          </a>
+          <p>
+            <a href="javascript:;" onClick={this.props.handleClick} className={this.props.visible ? Styles.red : Styles.blue}>
+              Click Me
+            </a>
+          </p>
         </h1>
       </div>
     );
